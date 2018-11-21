e634798da790b90663d73da7adf9ef1015bc083c
Mariela Kasovska <mariela.kasovska@sumup.com>
2018-11-21 15:21:03 +0200

    Add answers

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -6,30 +6,35 @@ import Result from "./Result";
 export default class Game extends Component {
   state = {
     currentQuestionIndex: 0,
-    isGameFinished: false
+    isGameFinished: false,
+    result: []
   };
 
-  handleAnswerSubmit = answer => {
-    const { currentQuestionIndex } = this.props;
-    const shouldChangeIndex =
-      this.state.currentQuestionIndex + 1 > questions.length - 1;
+  handleAnswerSubmit = result => {
     debugger;
+    const { currentQuestionIndex } = this.state;
+    const shouldChangeIndex =
+      this.state.currentQuestionIndex + 1 >= questions.length - 1;
+
     this.setState({
       currentQuestionIndex: shouldChangeIndex
         ? currentQuestionIndex + 1
         : currentQuestionIndex,
-      isGameFinished: true
+      isGameFinished: true,
+      result: this.state.result.concat(result)
     });
   };
 
   render() {
+    const { result } = this.state;
+    
     return (
       <div>
         {questions.map(question => (
           <Question {...question} submitAnswer={this.handleAnswerSubmit} />
         ))}
+        { result && <Result result={result}/>}
       </div>
-      <Result/>
     );
   }
 }
