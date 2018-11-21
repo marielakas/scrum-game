4740f12bbfde39ecb3a4751ea619a0e916ae11e9
Fernando <fernandofleuryg@gmail.com>
2018-11-21 15:12:41 +0200

    Remove Question from index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Question from "./Game/Question";
-
 import Game from "./Game";
 
 import "./styles.css";
@@ -12,7 +10,6 @@ function App() {
     <div className="App">
       <h1 className="title">Scrum fun game</h1>
       <Game />
-      <Question question="Why scrum" options={["a", "b", "c"]} />
     </div>
   );
 }
