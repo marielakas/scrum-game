a8d1d71d277192ae76a0fd7a6ce840906f2a1841
Fernando <fernandofleuryg@gmail.com>
2018-11-21 15:29:59 +0200

    Update questions

--- src/questions.js
+++ src/questions.js
@@ -1,14 +1,32 @@
 export default [
   {
     id: 0,
-    question: "Is scrum good?",
-    options: ["no", "yes"],
-    correctAnswer: "yes"
+    question: "In an organization that embraces Agile values, who would be responsible for tool selection and configuration?",
+    options: ["The teams, who would have to coordinate with each other", "The ScrumMasters, who would have to coordinate with each other"],
+    correctAnswer: "The teams, who would have to coordinate with each other"
   },
   {
     id: 1,
-    question: "Why you use scrum",
-    options: ["idk", "yes"],
-    correctAnswer: "idk"
+    question: "What is a good size for a sprint task?",
+    options: ["One person per day or less, so other team members can easily detect when a task is stuck", "2-3 people 2-3 days, so that every product backlog item equals one sprint task"],
+    correctAnswer: "One person per day or less, so other team members can easily detect when a task is stuck"
+  },
+  {
+    id: 2,
+    question: "Does scrum have a role called “project manager”?",
+    options: ["No", "Yes"],
+    correctAnswer: "No"
+  },
+  {
+    id: 3,
+    question: "Some Scrum teams fully embrace Agile technical practices. How often do these teams integrate their work and rerun regression tests?",
+    options: ["Continuously as things change; potentially many times per day", "Only at the end of each sprint", 'Once pre day (or night)'],
+    correctAnswer: "Continuously as things change; potentially many times per day"
+  },
+  {
+    id: 4,
+    question: "Which is not an effective way to encourage collaboration between the scrum development team and product owner?",
+    options: ["Teach the team to talk in terms of business needs and objectives", "Listen to their communications", "Act as a go-between for them", "Teach the product owner about technologies the team uses"],
+    correctAnswer: "Act as a go-between for them"
   }
 ];
