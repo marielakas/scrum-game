c59bc17e4e285708a693d9a095a9b93eafbe8897
Fernando <fernandofleuryg@gmail.com>
2018-11-22 11:53:24 +0200

    Update question

--- src/Game/Question.js
+++ src/Game/Question.js
@@ -1,46 +1,18 @@
 import React from "react";
 
 const Question = ({ question, options, submitAnswer }) => (
-  <div class="question">
+  <div className="question">
     <h2>{question}</h2>
 
-    <div class="options">
+    <div className="options">
       {options.map(option => (
-        <div>
-          <input
-            type="radio"
-            name={`option-${question
-              .toLowerCase()
-              .trim()
-              .replace(/\s/g, "-")}`}
-            id={option}
-            value={option}
-            key={option}
-          />
-          <span class="option">{option}</span>
+        <div className="option" key={option}>
+          <button onClick={() => submitAnswer(option)} class="btn btn-light">
+            {option}
+          </button>
         </div>
       ))}
     </div>
-
-    <button
-      class="btn btn-primary"
-      onClick={() => {
-        const answer = document.querySelector(
-          `input[name="option-${question
-            .toLowerCase()
-            .trim()
-            .replace(/\s/g, "-")}"]:checked`
-        );
-
-        if (!answer) {
-          return;
-        }
-
-        return submitAnswer(answer.value);
-      }}
-    >
-      submit answer
-    </button>
   </div>
 );
 
