d70c2dd013221d3458626679cebcf95aca77b4dc
Fernando <fernandofleuryg@gmail.com>
2018-11-22 12:11:35 +0200

    Re introduce beautiful title

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ import "./styles.css";
 function App() {
   return (
     <div className="container-fluid">
+      <div class="row">
+        <div class="col">
+        <marquee direction="down" width="100%" height="200" behavior="alternate">
+          <marquee behavior="alternate">
+          <h1 className="title">Scrum fun game (real fun) v2<br /> (now even more fun)</h1>
+          </marquee>
+        </marquee>
+        </div>
+      </div>
       <div className="row">
         <div className="col">
           <Game />
