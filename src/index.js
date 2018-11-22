e5005942a8b7b1e1affe259fb554f438e9c01664
Fernando <fernandofleuryg@gmail.com>
2018-11-22 10:52:29 +0200

    Update title

--- src/index.js
+++ src/index.js
@@ -7,18 +7,14 @@ import "./styles.css";
 
 function App() {
   return (
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col">
-        <marquee direction="down" width="100%" height="200" behavior="alternate">
+    <div className="container-fluid">
+      <marquee className="" style={{ position: 'absolute', top: 0, left: 0, zIndex: 100 }} direction="down" width="100%" height="100%" behavior="alternate">
           <marquee behavior="alternate">
-          <h1 class="title">Scrum fun game (real fun)</h1>
+          <h1 className="title">Scrum fun game (real fun) v2<br /> (now even more fun)</h1>
           </marquee>
         </marquee>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
+      <div className="row">
+        <div className="col">
           <Game />
         </div>
       </div>
