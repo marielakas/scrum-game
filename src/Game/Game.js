7f739a94b11559965d6f944c01f0496584401667
Martin Inkyov <martin.inkyov@sumup.com>
2018-11-22 12:17:08 +0200

    Display result and reset button

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -3,35 +3,53 @@ import questions from "../questions";
 import Question from "./Question";
 import Result from "./Result";
 
+const initialState = {
+  currentQuestionIndex: 0,
+  isGameFinished: false,
+  correctAnswers: 0
+};
+
 export default class Game extends Component {
-  state = {
-    currentQuestionIndex: 0,
-    isGameFinished: false,
-    result: []
-  };
+  constructor(props){
+    super(props);
+    this.state = initialState;
+  }
+
+  resetQuiz = () => {
+    this.setState(initialState);
+  }
+
+  isAnswerCorrect = (currentQuestionIndex, answer) => {
+    if (questions[currentQuestionIndex].correctAnswer === answer) {
+      return true;
+    }
+  }
 
   handleAnswerSubmit = answer => {
-    const { currentQuestionIndex, result } = this.state;
+    const { currentQuestionIndex, correctAnswers } = this.state;
     const hasNextQuestion =
       this.state.currentQuestionIndex + 1 >= questions.length - 1;
+
+    const isAnswerCorrect = this.isAnswerCorrect(currentQuestionIndex, answer);
 
     this.setState({
       currentQuestionIndex: !hasNextQuestion
         ? currentQuestionIndex + 1
         : currentQuestionIndex,
       isGameFinished: hasNextQuestion,
-      result: result.concat({ id: currentQuestionIndex, answer})
+      correctAnswers: isAnswerCorrect ? correctAnswers + 1 : correctAnswers
     });
   };
 
   render() {
-    const { result, isGameFinished, currentQuestionIndex } = this.state;
+    const { correctAnswers, isGameFinished, currentQuestionIndex } = this.state;
     const currentQuestion = questions[currentQuestionIndex];
 
     return (
       <div>
         <Question {...currentQuestion} submitAnswer={this.handleAnswerSubmit} />
-        { isGameFinished ? <Result result={result}/> : null}
+        { isGameFinished ? <Result numberOfRightAnswers={correctAnswers}/> : null}
+        { isGameFinished && <button onClick={this.resetQuiz}>Reset</button>}
       </div>
     );
   }
--- src/Game/Result.js
+++ src/Game/Result.js
@@ -2,22 +2,11 @@ import React from "react";
 
 import hardcodeQuestions from "../questions";
 
-const Result = ({ result }) => {
-  let correctAnswers = 0;
-  let wrongAnswers = 0;
-  hardcodeQuestions.forEach(question => {
-    if(result[question.id]) {
-      if (question.correctAnswer === result[question.id].answer) {
-        correctAnswers += 1;
-      } else {
-        wrongAnswers += 1;
-      }
-    }
-  });
+const Result = ({ numberOfRightAnswers }) => {
 
   return (
     <div>
-      You got {correctAnswers} correct and {wrongAnswers} wrong.
+      You got {numberOfRightAnswers}/{hardcodeQuestions.length}.
     </div>
   );
 };
