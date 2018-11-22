e26399f33b8e015a101b613d1d122f9ccde2fb48
Fernando <fernandofleuryg@gmail.com>
2018-11-22 12:06:51 +0200

    Update Question component

--- src/Game/Question.js
+++ src/Game/Question.js
@@ -1,13 +1,30 @@
 import React from "react";
 
-const Question = ({ question, options, submitAnswer }) => (
+/**
+ * This is how a result question should look like:
+ * <Question question="..." options={[]} readonly rightOption="" wrongOption="" />
+ */
+
+const getClassNames = (option, rightOption, wrongOption, readonly) => {
+  if (readonly && rightOption === option) {
+    return 'btn btn-success';
+  }
+
+  if (readonly && wrongOption === option) {
+    return 'btn btn-danger';
+  }
+
+  return 'btn btn-light';
+}
+
+const Question = ({ question, options, submitAnswer, readonly, rightOption, wrongOption }) => (
   <div className="question">
     <h2>{question}</h2>
 
     <div className="options">
       {options.map(option => (
         <div className="option" key={option}>
-          <button onClick={() => submitAnswer(option)} class="btn btn-light">
+          <button disabled={readonly} onClick={() => submitAnswer(option)} class={getClassNames(option, rightOption, wrongOption, readonly)}>
             {option}
           </button>
         </div>
