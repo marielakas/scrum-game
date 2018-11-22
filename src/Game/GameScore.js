db9a0d54686213c69abd18a36214a49e83b815b2
Diyan Chuburov <diyan.chuburov@sumup.com>
2018-11-22 12:25:29 +0200

    Add sorting and few fixes to scores

--- src/Game/GameScore.js
+++ src/Game/GameScore.js
@@ -2,18 +2,23 @@ import React, { Component } from "react";
 
 import { getScores, saveGameScore, resetGameScore } from "./GameService";
 
+const prepScores = scores => {
+  scores.sort((a, b) => b.score - a.score);
+
+  return scores;
+};
+
 const GameScores = ({ scores }) => (
-  <div>
-    {scores.map(({ username, score }, index) => (
+  <div style={{ marginBottom: "20px" }}>
+    {prepScores(scores).map(({ username, score }, index) => (
       <div key={index}>
-        <span>{username}</span>
-        <span>{score}</span>
+        <div style={{ float: "left" }}>{username}</div>
+        <div style={{ float: "right" }}>{score}</div>
+        <div style={{ clear: "both" }} />
       </div>
     ))}
   </div>
 );
-
-console.log(getScores());
 
 export default class GameScoreContainer extends Component {
   state = { scores: getScores() };
@@ -42,7 +47,7 @@ export default class GameScoreContainer extends Component {
     console.log(scores);
 
     return (
-      <div>
+      <div style={{ width: "300px", margin: "0 auto" }}>
         <GameScores scores={scores} />
         <button onClick={this.handleAddScore}>Add Score</button>
         <button onClick={this.handleResetStore}>Reset Score</button>
